decfbb64478c4c8f63c6a01732d86e42b22d489a
xiaoyu-liu <37421777+xiaoyu-liu@users.noreply.github.com>
2018-03-15 19:08:01 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+import os
+
+# data analysis and wrangling
+import pandas as pd
+import numpy as np
+import random as rnd
+
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+                               #matplotlib inline
+
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Input Dataset
+os.chdir("C:\\Summer\\Databases by Python - Coursera\\Kaggle Project\\Titanic-180109")
+train_df = pd.read_csv('train.csv')
+test_df = pd.read_csv('test.csv')
+combine = [train_df, test_df]
