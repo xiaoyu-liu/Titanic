e7b12b95f056f3b6c24ef428a7e20ba30bb06a26
xiaoyu-liu <37421777+xiaoyu-liu@users.noreply.github.com>
2018-03-17 18:05:41 -0400

    Create MLmodel.py

---
+++ MLmodel.py
@@ -0,0 +1,10 @@
+# Visualization tool
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Machine Learning algorithms
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
