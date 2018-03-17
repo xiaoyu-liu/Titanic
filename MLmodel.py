ce7b713862cf3e538979251a9d76d78ef3463b91
xiaoyu-liu <37421777+xiaoyu-liu@users.noreply.github.com>
2018-03-17 18:07:05 -0400

    Update MLmodel.py

--- MLmodel.py
+++ MLmodel.py
@@ -8,3 +8,75 @@ from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
+
+X_train = train_df.drop("Survived", axis=1)
+Y_train = train_df["Survived"]
+X_test  = test_df
+
+##------------------- Logistic Regression----------------------------
+logreg = LogisticRegression()
+logreg.fit(X_train, Y_train)
+Y_pred = logreg.predict(X_test)
+acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
+acc_log
+# See the importance of features
+coeff_df = pd.DataFrame(train_df.columns.delete(0))
+coeff_df.columns = ['Feature']
+coeff_df["Importance"] = pd.Series(logreg.coef_[0])
+coeff_df["Importance%"] = 100.0 * (abs(coeff_df["Importance"]) \
+        / abs(coeff_df["Importance"]).sum())
+coeff_df.sort_values(by='Importance%', ascending=False)
+
+##------------------------------ SVC ---------------------------------
+##-------------------- Support Vector Machines -----------------------
+svc = SVC()
+svc.fit(X_train, Y_train)
+Y_pred = svc.predict(X_test)
+acc_svc = round(svc.score(X_train, Y_train) * 100, 2)
+acc_svc
+
+##---------------------------- K-nn ----------------------------------
+##------------------k-Nearest Neighbors algorithm --------------------
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(X_train, Y_train)
+Y_pred = knn.predict(X_test)
+acc_knn = round(knn.score(X_train, Y_train) * 100, 2)
+acc_knn
+
+##---------------------- Decision Tree -------------------------------
+decision_tree = DecisionTreeClassifier()
+mytree = decision_tree.fit(X_train, Y_train)
+Y_pred = decision_tree.predict(X_test)
+acc_decision_tree = round(decision_tree.score(X_train, Y_train) * 100, 2)
+acc_decision_tree
+
+from sklearn import tree
+with open("treeTitanic.dot","w") as f:
+    f = tree.export_graphviz(mytree,out_file=f)
+
+
+from sklearn.externals.six import StringIO  
+from IPython.display import Image  
+import pydotplus
+dot_data = StringIO()
+tree.export_graphviz(decision_tree, out_file=dot_data,  
+                filled=True, rounded=True,
+                special_characters=True)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+Image(graph.create_png())
+
+##---------------------- Random Forest -------------------------------
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(X_train, Y_train)
+Y_pred = random_forest.predict(X_test)
+random_forest.score(X_train, Y_train)
+acc_random_forest = round(random_forest.score(X_train, Y_train) * 100, 2)
+acc_random_forest
+
+##*********************** Summary of results ************************
+models = pd.DataFrame({
+    'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression', 
+              'Random Forest', 'Decision Tree'],
+    'Score': [acc_svc, acc_knn, acc_log, 
+              acc_random_forest, acc_decision_tree]})
+models.sort_values(by='Score', ascending=False)
